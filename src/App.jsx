4201913927305469d68472395054f3fc5a07e3bd
shohan12727 <aishohan001@gmail.com>
2025-04-08 09:40:52 +0600

    fucking done with this shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-// import Header from './Header'
-// import Footer from './Footer'
-// import Food from './Food'
+import Header from './Header'
+import Footer from './Footer'
+import Food from './Food'
 
 import Button from "./Button"
 
-// import Card from "./Card"
+import Card from "./Card"
 
 
 function App() {
@@ -13,13 +13,13 @@ function App() {
 
   return (
     <>
-   {/* <Header></Header>
+   <Header></Header>
    <Food></Food>
-   <Footer></Footer> */}
-   {/* <Card></Card>
+   <Footer></Footer> 
+    <Card></Card>
    <Card></Card>
    <Card></Card>
-   <Card></Card> */}
+   <Card></Card>
   <Button></Button>
    </>
   )
